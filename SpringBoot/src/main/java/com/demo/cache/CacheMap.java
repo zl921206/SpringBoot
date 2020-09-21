707dd06b24b59d7615b8f3a03e8cc2e1ff05008d
zhanglei_18 <zhanglei_18@139.com>
2020-09-21 11:28:07 +0800

    单例优化

--- SpringBoot/src/main/java/com/demo/cache/CacheMap.java
+++ SpringBoot/src/main/java/com/demo/cache/CacheMap.java
@@ -20,10 +20,29 @@ public class CacheMap {
     /**
      * 定义饿汉式单例
      */
-    private static CacheMap instance = new CacheMap();
+//    private static CacheMap instance = new CacheMap();
+//    public static CacheMap getInstance() {
+//        if (instance == null) {
+//            instance = new CacheMap();
+//        }
+//        return instance;
+//    }
+
+    /**
+     * 定义线程安全的懒汉式单例
+     * 多线程时使用volatile关键字: 有两个作用：1：保持线程可见性；2：指令重排序
+     */
+    private static volatile CacheMap instance = null;
     public static CacheMap getInstance() {
+        // 先判断对象是否为空（作用：降低服务开销，防止多线程环境，所有线程都直接进行同步等待锁，造成资源占用过大）
         if (instance == null) {
-            instance = new CacheMap();
+            //同步锁: 锁当前对象
+            synchronized(CacheMap.class){
+                // 再判断一次对象是否为空（保证该instanceLazy实例仅被初始化一次）
+                if(instance == null){
+                    instance = new CacheMap();
+                }
+            }
         }
         return instance;
     }
