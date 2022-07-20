68c649ba17c0345aea60d5bd4285e7adfff28e8d
zhanglei_18 <zhanglei_18@139.com>
2022-07-20 17:27:55 +0800

    xxl-job集成

--- SpringBoot/src/main/java/com/demo/job/JobHandler.java
+++ SpringBoot/src/main/java/com/demo/job/JobHandler.java
@@ -2,7 +2,6 @@ package com.demo.job;
 
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.annotation.XxlJob;
-import com.xxl.job.core.log.XxlJobLogger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -20,12 +19,12 @@ public class JobHandler {
      */
     @XxlJob("jobHandler")
     public ReturnT<String> execute(String param) {
-        XxlJobLogger.log("任务开始执行......");
+        log.info("任务开始执行......");
         /**
          * 此处编写需要执行的业务代码
          */
         // 调用业务逻辑代码
-        XxlJobLogger.log("任务结束执行......");
+        log.info("任务结束执行......");
         return ReturnT.SUCCESS;
     }
 }
